ece8d0cfae2461125aa634f38236ace9a6b12c42
Xie <xcgoner1108@gmail.com>
2019-06-21 16:46:48 -0700

    local sgd with horovod

--- scripts/machine_translation/train_transformer_local_sgd_hvd.py
+++ scripts/machine_translation/train_transformer_local_sgd_hvd.py
@@ -472,6 +472,7 @@ def train():
                 # logging.info('[{}] [batch {}] before step'.format(rank, batch_id))
 
                 is_sync = trainer.step(float(loss_denom) / args.batch_size / 100.0)
+                mx.nd.waitall()
 
                 # mx.nd.waitall()
                 # logging.info('[{}] [batch {}] after step'.format(rank, batch_id))
