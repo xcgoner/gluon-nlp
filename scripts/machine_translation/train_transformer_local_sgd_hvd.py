20d1df041e0093c03a4fdf7017937735570ed8ad
Xie <xcgoner1108@gmail.com>
2019-06-25 00:07:11 -0700

    local sgd with horovod

--- scripts/machine_translation/train_transformer_local_sgd_hvd.py
+++ scripts/machine_translation/train_transformer_local_sgd_hvd.py
@@ -429,7 +429,7 @@ def train():
         # sync params
         # mx.nd.waitall()
         trainer.allreduce_params()
-        # trainer.allreduce_states()
+        trainer.allreduce_states()
         # allreduce_params(trainer)
         # allreduce_states(trainer)
         mx.nd.waitall()
