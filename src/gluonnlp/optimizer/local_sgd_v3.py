532ff79289212617351475199c40b10ff0f32ef8
Xie <xcgoner1108@gmail.com>
2019-07-12 12:39:21 -0700

    adaptive step size for local sgd

--- src/gluonnlp/optimizer/local_sgd_v3.py
+++ src/gluonnlp/optimizer/local_sgd_v3.py
@@ -137,7 +137,7 @@ class LocalSGDTrainerV3(mx.gluon.Trainer):
     def reset_adam_counter(self, t):
         for i, param in enumerate(self._params):
             if param.grad_req != 'null':
-                trainer._optimizer._index_update_count[i] = t
+                self._optimizer._index_update_count[i] = t
 
     def init_states(self):
         """Initialize states (momentum for sgd_mon, or mean/var for adam) in the KVStore, for local sgd
