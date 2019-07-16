abd6ed1d09426c5b59be3ea24cc1bb16d345b822
Xie <xcgoner1108@gmail.com>
2019-07-16 14:18:42 -0700

    bert local sgd hvd

--- scripts/bert/local_sgd_hvd.py
+++ scripts/bert/local_sgd_hvd.py
@@ -68,6 +68,44 @@ class FP16DistributedLocalSGDTrainer(hvd.DistributedTrainer):
     #             self._allreduce_params()
     #             # self._allreduce_states()
 
+    def update(self, batch_size, ignore_stale_grad=False):
+        """Makes one step of parameter update.
+        Should be called after `autograd.backward()` and outside of `record()` scope,
+        and after `trainer.update()`.
+        For normal parameter updates, `step()` should be used, which internally calls
+        `allreduce_grads()` and then `update()`. However, if you need to get the reduced
+        gradients to perform certain transformation, such as in gradient clipping, then
+        you may want to manually call `allreduce_grads()` and `update()` separately.
+        Parameters
+        ----------
+        batch_size : int
+            Batch size of data processed. Gradient will be normalized by `1/batch_size`.
+            Set this to 1 if you normalized loss manually with `loss = mean(loss)`.
+        ignore_stale_grad : bool, optional, default=False
+            If true, ignores Parameters with stale gradient (gradient that has not
+            been updated by `backward` after last step) and skip update.
+        """
+        if not self._kv_initialized:
+            self._init_kvstore()
+        if self._params_to_init:
+            self._init_params()
+        assert not (self._kvstore and self._update_on_kvstore), \
+                'update() when parameters are updated on kvstore ' \
+                'is not supported. Try setting `update_on_kvstore` ' \
+                'to False when creating trainer.'
+
+        self._check_and_rescale_grad(self._scale / batch_size)
+        self._update(ignore_stale_grad)
+
+        if self._local_sgd > 1:
+            # local sgd
+            self._local_sgd_counter += 1
+            if self._local_sgd_counter == self._local_sgd:
+                self._local_sgd_counter = 0
+                # synchronization
+                # self._allreduce_params()
+                self._allreduce_states()
+
     def _allreduce_params(self):
         # print("_allreduce_params")
         mx.nd.waitall()
