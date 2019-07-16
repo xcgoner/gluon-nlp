a558fa5a8e3666eddc5d969e96972fa420ee80e4
Xie <xcgoner1108@gmail.com>
2019-07-16 10:41:09 -0700

    bert local sgd hvd

--- src/gluonnlp/optimizer/local_sgd_hvd.py
+++ src/gluonnlp/optimizer/local_sgd_hvd.py
@@ -46,39 +46,16 @@ class DistributedLocalSGDTrainer(hvd.DistributedTrainer):
             self._local_sgd = local_sgd
         self._local_sgd_counter = 0
 
-
-    def step(self, batch_sizes, ignore_stale_grad=False):
-        """Makes one step of parameter update. Should be called after
-        `autograd.backward()` and outside of `record()` scope.
-
-        For normal parameter updates, `step()` should be used, which internally calls
-        `allreduce_grads()` and then `update()`. However, if you need to get the reduced
-        gradients to perform certain transformation, such as in gradient clipping, then
-        you may want to manually call `allreduce_grads()` and `update()` separately.
-
-        Parameters
-        ----------
-        batch_sizes : [int]
-            Batch size of data processed. Gradient will be normalized by `1/batch_size`.
-            Set this to 1 if you normalized loss manually with `loss = mean(loss)`.
-        ignore_stale_grad : bool, optional, default=False
-            If true, ignores Parameters with stale gradient (gradient that has not
-            been updated by `backward` after last step) and skip update.
-        """
-        rescale_grad = self._scale / batch_size
-        self._check_and_rescale_grad(rescale_grad)
-
-        if not self._kv_initialized:
-            self._init_kvstore()
-        if self._params_to_init:
-            self._init_params()
-
+    def _allreduce_grads(self):
+        # sort needed for Python < 3.6 is not guaranteed
         if self._local_sgd == 1:
-            # if not local sgd
-            self._allreduce_grads()
+            for i, param in enumerate(sorted(self._params, key=lambda p: p.name)):
+                if param.grad_req != 'null':
+                    hvd.allreduce(param.list_grad()[0], average=False,
+                    name=str(i), priority=-i)
 
-        self._update(ignore_stale_grad)
-
+    def _update(self, ignore_stale_grad=False):
+        super(DistributedLocalSGDTrainer, self)._update(ignore_stale_grad=ignore_stale_grad)
         if self._local_sgd > 1:
             # local sgd
             self._local_sgd_counter += 1
@@ -87,10 +64,10 @@ class DistributedLocalSGDTrainer(hvd.DistributedTrainer):
                 # synchronization
                 self._allreduce_params()
                 self._allreduce_states()
-                # indicate that the parameters are synchronized in the current iteration
-                return True
-            return False
-        return True
+        #         # indicate that the parameters are synchronized in the current iteration
+        #         return True
+        #     return False
+        # return True
 
     def allreduce_params(self):
         """For each parameter, reduce the gradients from different contexts.
