e21fb367207bd8394ce385f7559d0166cf4caabe
Xie <xcgoner1108@gmail.com>
2019-07-24 14:42:11 -0700

    bucketing

--- scripts/bert/run_pretraining_bucket_benchmark.py
+++ scripts/bert/run_pretraining_bucket_benchmark.py
@@ -159,7 +159,8 @@ def train(data_train, model, nsp_loss, mlm_loss, vocab_size, ctx, store):
             if batch_num == 200:
                 break
 
-            for data in data_list:
+            for data, _ in data_list:
+                sample_shape = data[0].shape
                 num_samples = data[0].shape[0]
                 dummy_pad = mx.nd.zeros((num_samples, 1), ctx[0], dtype='int32')
                 data[0] = mx.nd.concat(data[0], dummy_pad, dim=1)
@@ -183,7 +184,7 @@ def train(data_train, model, nsp_loss, mlm_loss, vocab_size, ctx, store):
                 benchmark_latency_array = np.array(benchmark_latency_list)
                 min_latency = np.asscalar(np.min(benchmark_latency_array))
                 max_latency = np.asscalar(np.max(benchmark_latency_array))
-                logging.info("batch_num={}, batch_size={}, latency={}, avg={}, std={}, min={}, max={}, gap={}".format(batch_num, data_list[0].shape, latency, np.asscalar(np.mean(benchmark_latency_array)), np.asscalar(np.std(benchmark_latency_array)), min_latency, max_latency, max_latency-min_latency))
+                logging.info("batch_num={}, batch_size={}, latency={}, avg={}, std={}, min={}, max={}, gap={}".format(batch_num, sample_shape, latency, np.asscalar(np.mean(benchmark_latency_array)), np.asscalar(np.std(benchmark_latency_array)), min_latency, max_latency, max_latency-min_latency))
             batch_num += 1
         break
     
