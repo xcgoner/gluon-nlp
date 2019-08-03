ff040b4e09df65ec95e59f4bfd1d523472430942
Xie <xcgoner1108@gmail.com>
2019-08-02 19:05:49 -0700

    debug local sgd hvd

--- scripts/bert/local_sgd_hvd_v2.py
+++ scripts/bert/local_sgd_hvd_v2.py
@@ -110,7 +110,7 @@ class FP16DistributedLocalSGDTrainerV2(hvd.DistributedTrainer):
                 allreduce_(self._updaters[0].states[i][1], average=True,
                                 name=str(i), priority=-i, 
                                 local_reduction = False, 
-                                cross_only = True)
+                                cross_only = False)
                 # copy fp32 weight to fp16 weight, assume using hvd with single GPU per process
                 # self._updaters[0].states[i][1].copyto(param.list_data()[0])
         # sync mean and var
