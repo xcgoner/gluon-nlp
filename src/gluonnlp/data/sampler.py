fad44d5a568280504377e7c7e408b8c4225e65ab
cx2 <cx2@illinois.edu>
2019-09-04 15:52:25 -0500

    transformer hvd

--- src/gluonnlp/data/sampler.py
+++ src/gluonnlp/data/sampler.py
@@ -20,7 +20,7 @@
 (e.g. in the order sorted by length). They can also be used to perform bucketing
 for speeding up the processing of variable-length sequences."""
 __all__ = ['ConstWidthBucket', 'LinearWidthBucket', 'ExpWidthBucket',
-           'SortedSampler', 'FixedBucketSampler', 'SortedBucketSampler', 'SplitSampler']
+           'SortedSampler', 'FixedBucketSampler', 'SortedBucketSampler', 'SplitSampler', 'SplitFixedBucketSampler']
 
 import math
 import warnings
