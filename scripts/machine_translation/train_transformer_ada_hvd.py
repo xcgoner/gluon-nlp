d25d51e242dc1dd0e34884720570193dde77363f
cx2 <cx2@illinois.edu>
2019-09-09 15:36:39 -0500

    transformer ada hvd

--- scripts/machine_translation/train_transformer_ada_hvd.py
+++ scripts/machine_translation/train_transformer_ada_hvd.py
@@ -347,7 +347,7 @@ def train():
             debug_array_1_np = debug_array_1.asnumpy()
             if rank==0:
                 logging.info('[Epoch {} Batch {}/{}], src_wc={}, tgt_wc={}, bs={}'.format(epoch_id, batch_id + 1, len(train_data_loader), 
-                                                                                                 debug_array_1_np[0], debug_array_1_np[1], debug_array_1_np[2], debug_array_1_np[3]))
+                                                                                                 debug_array_1_np[0], debug_array_1_np[1], debug_array_1_np[2]))
             debug_array_2 = mx.nd.zeros((num_workers, ))
             debug_array_2[rank] = float(ls.asscalar())
             hvd.allreduce_(debug_array_2, average=False, name='debug_array_2', priority=0)
