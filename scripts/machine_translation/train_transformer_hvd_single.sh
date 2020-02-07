ff38e7c21e953048dd66bdde1e271133758a7594
unknown <xcgoner1108@gmail.com>
2020-02-06 21:25:25 -0600

    transformer hvd

--- scripts/machine_translation/train_transformer_hvd_single.sh
+++ scripts/machine_translation/train_transformer_hvd_single.sh
@@ -30,6 +30,6 @@ cd /homes/cx2/src/localadam/uai2020/gluon-nlp/scripts/machine_translation
 mpirun -np 2 -host localhost -ppn 2 -genv I_MPI_PIN_DOMAIN auto:compact \
                        python train_transformer_hvd.py --dataset WMT2014BPE \
                        --src_lang en --tgt_lang de --batch_size 10800 \
-                       --optimizer adam --num_accumulated 4 --lr 2.0 --warmup_steps 4000 \
+                       --optimizer adam --num_accumulated 16 --lr 2.0 --warmup_steps 4000 \
                        --save_dir transformer_en_de_u512 --epochs 30 --scaled \
                        --average_start 5 --num_buckets 20 --bucket_scheme exp --bleu 13a --log_interval 10 2>&1 | tee -a $watchfile
