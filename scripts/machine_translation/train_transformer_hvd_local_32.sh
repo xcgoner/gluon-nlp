58c4abc89b114dfb6d4e28d26dcfa79599f444b9
unknown <xcgoner1108@gmail.com>
2020-02-28 12:11:44 -0600

    local adam v3

--- scripts/machine_translation/train_transformer_hvd_local_32.sh
+++ scripts/machine_translation/train_transformer_hvd_local_32.sh
@@ -31,7 +31,7 @@ cd /homes/cx2/src/localadam/uai2020/gluon-nlp/scripts/machine_translation
 mpirun -np 32 -machinefile $PBS_O_WORKDIR/hostfile_local_32 -ppn 2 -genv I_MPI_PIN_DOMAIN auto:compact \
                        python train_transformer_hvd_local_v3.py --dataset WMT2014BPE \
                        --src_lang en --tgt_lang de --batch_size 10800 \
-                       --optimizer localadamv3 --num_accumulated 1 --lr 1.5 --warmup_steps 3000 \
+                       --optimizer localadamv3 --num_accumulated 1 --lr 3.0 --warmup_steps 4000 \
                        --save_dir transformer_en_de_u512 --epochs 30 --scaled \
                        --average_start 5 --num_buckets 20 --bucket_scheme exp --bleu 13a --log_interval 16 \
                        --local_sgd_interval 4 2>&1 | tee -a $watchfile
